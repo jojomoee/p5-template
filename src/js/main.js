10093323b8bfc4899c1724f06c67cd6131cc3a55
Johannes Moe <johannesmoehrl@protonmail.com>
2022-11-29 03:26:42 +0100

    template

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+//P5 SKETCH
+
+let p5c = document.getElementById('c2');
+let width = p5c.offsetWidth;
+let height = p5c.offsetHeight;
+
+const s = (sketch) => {
+  sketch.setup = () => {
+    sketch.createCanvas(width, height);
+  };
+
+  sketch.draw = () => {
+    sketch.background(0);
+    sketch.fill(255);
+    sketch.rect(width / 2 - 25, height / 2 - 25, 50, 50);
+  };
+};
+
+let myp5 = new p5(s, p5c);
+console.log(p5c.offsetWidth);
